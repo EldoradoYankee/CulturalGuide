265e8d4589ee66b4a26a7b7b1310677459f2abba
eldoradoyankee <adi2410@hotmail.com>
2025-12-04 18:16:50 +0100

    migrations

---
+++ CulturalGuideBACKEND/Migrations/20251201163628_AddMissingUserFields.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CulturalGuideBACKEND.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingUserFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVerified",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "JwtToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetTokenExpires",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVerified",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "JwtToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetTokenExpires",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationToken",
+                table: "Users");
+        }
+    }
+}
